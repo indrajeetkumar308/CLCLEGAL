d310ae0ee27667b6ca8c037768fbe2b11040182c
indrajeetkumar308 <indrajeetkumar308@users.noreply.github.com>
2026-09-25 15:14:14 +0000

    Make site installable as CLC app (manifest, icons, service worker)

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE='clc-v1';
+const ASSETS=['/','/index.html','/manifest.webmanifest','/icons/icon-192.png','/icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{
+  const r=e.request; if(r.method!=='GET'||new URL(r.url).origin!==location.origin)return;
+  e.respondWith(fetch(r).then(res=>{const cp=res.clone();caches.open(CACHE).then(c=>c.put(r,cp));return res}).catch(()=>caches.match(r).then(m=>m||caches.match('/index.html'))));
+});
